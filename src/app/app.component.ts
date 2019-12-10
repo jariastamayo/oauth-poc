99e55199b31cfc9b5e456e1d6fe0488002fd3efd
Juan Camilo Arias Tamayo <jariastamayo@deloitte.com>
2019-12-10 06:30:48 -0600

    Getting tokens from Azure.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { OAuthService, JwksValidationHandler } from 'angular-oauth2-oidc';
+import { authConfig } from './auth.config';
+import {filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'poc-oauth';
+
+  constructor(private oauthService: OAuthService) {
+    this.configure();
+  }
+
+  configure() {
+    console.log(window.location.href);
+    this.oauthService.configure(authConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    // this.oauthService.setStorage('')
+    // this.oauthService.tryLogin();
+    console.log(window.location.href);
+    // this.oauthService.loadDiscoveryDocumentAndLogin();
+    this.oauthService.events
+        .pipe(filter((e: any) => e.type === 'token_received'))
+        .subscribe(() => console.log('HERE'));
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+
+  public login() {
+    // this.oauthService.initLoginFlow();
+    this.oauthService.initImplicitFlow();
+  }
+
+  public logoff() {
+    this.oauthService.logOut();
+  }
+
+  public getName() {
+    let claims = this.oauthService.getIdToken();
+    console.log('ID');
+    console.log(claims);
+  }
 }
